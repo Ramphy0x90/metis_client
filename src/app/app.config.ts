7035c87f8dbb41b1eb619ff2464109ad24cd3f21
Ramphy Aquino Nova <ramphy_an@outlook.com>
2025-09-05 22:35:54 +0200

    min: structure

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,24 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import {
+	ApplicationConfig,
+	provideBrowserGlobalErrorListeners,
+	provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideStore } from '@ngrx/store';
+import { provideHttpClient } from '@angular/common/http';
+import { identityReducer } from './store/identity';
+import { provideEffects } from '@ngrx/effects';
+import { IdentityEffects } from './store/identity/identity.effects';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
-  ]
+	providers: [
+		provideBrowserGlobalErrorListeners(),
+		provideZoneChangeDetection({ eventCoalescing: true }),
+		provideRouter(routes),
+		provideStore({ identity: identityReducer }),
+		provideEffects([IdentityEffects]),
+		provideHttpClient(),
+	],
 };
