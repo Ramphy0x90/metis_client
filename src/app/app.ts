f7971045d08d5024446013b42605b8471949f508
Ramphy Aquino Nova <ramphy_an@outlook.com>
2025-09-06 14:27:08 +0200

    min: app layout

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,23 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavigationComponent } from './components/navigation-component/navigation-component';
+import { Store } from '@ngrx/store';
+import { selectIsAuthenticated } from './store/identity';
+import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.scss'
+	selector: 'app-root',
+	imports: [CommonModule, RouterOutlet, NavigationComponent],
+	templateUrl: './app.html',
+	styleUrl: './app.scss',
 })
 export class App {
-  protected readonly title = signal('metis_client');
+	protected readonly title = signal('Metis');
+
+	constructor(private store: Store) {}
+
+	get isUserAuthenticated(): Observable<boolean> {
+		return this.store.select(selectIsAuthenticated);
+	}
 }
